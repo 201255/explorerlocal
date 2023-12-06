b6c5c11a6b44494e5bd75aa4d1040a631018f87f
201255 <201255@ids.upchiapas.edu.mx>
2023-12-06 03:04:22 -0600

    a

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Application} from 'express';
+import express, {Application, Request, Response} from 'express';
 import proxy from 'express-http-proxy';
 import morgan from 'morgan';
 
@@ -13,16 +13,22 @@ app.use(morgan('dev'));
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+
+
+
 //Review
-app.use('/api/v1/Review/', proxy('http://localhost:3003'));
+app.use('/api/v1/Review', proxy('http://service3-env.eba-mwncfzqa.us-east-1.elasticbeanstalk.com'));
 
 //User
-app.use('/api/v1/User/', proxy('http://localhost:3002'));
+app.use('/api/v1/User', proxy('http://servicio1-env.eba-aikdu2nx.us-east-1.elasticbeanstalk.com'));
 
 //Restaurant
-app.use('/api/v1/Menu/', proxy('http://localhost:3001'));
-app.use('/api/v1/Restaurant/', proxy('http://localhost:3001'));
+app.use('/api/v1/Menu', proxy('http://servicio2-env.eba-yv5qtqpq.us-east-1.elasticbeanstalk.com'));
+app.use('/api/v1/Restaurant', proxy('http://servicio2-env.eba-yv5qtqpq.us-east-1.elasticbeanstalk.com'));
 
+app.get('/rutine', (req: Request, res: Response) => {
+    res.status(200).send('Rutina ejecutáda con éxito todo chido');
+})
 
 app.listen(PORT,() => {
     signale.success(`Servidor corriendo en http://localhost:${PORT}`);
