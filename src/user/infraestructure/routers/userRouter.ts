8c5e6e6e44f0298bb0d002cb24713d79ec5b4d77
201255 <201255@ids.upchiapas.edu.mx>
2023-12-05 23:34:18 -0600

    second commit rutine

--- src/user/infraestructure/routers/userRouter.ts
+++ src/user/infraestructure/routers/userRouter.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { loginUserController, resgisterUserController, updatePasswordController, updateUserByIdController,usergetController } from "../dependencies";
 import { validateToken } from "../../../helpers/veryfyToken";
-
-
+import { Request, Response } from "express";
 
 export const userRoutes = express.Router();
+
+userRoutes.get('/rutine', (req: Request, res: Response) => {
+    res.status(200).send('Rutina ejecutáda con éxito');
+})
 userRoutes.get('/:uuid',usergetController.getRestaurantId.bind(usergetController));
 userRoutes.post('/',resgisterUserController.run.bind(resgisterUserController)) 
 
