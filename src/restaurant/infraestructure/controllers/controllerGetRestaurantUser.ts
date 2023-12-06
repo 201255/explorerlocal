69f451e859af5e3df6692ac0d950d3f78296f0ff
201255 <201255@ids.upchiapas.edu.mx>
2023-12-06 11:56:32 -0600

    https

--- src/restaurant/infraestructure/controllers/controllerGetRestaurantUser.ts
+++ src/restaurant/infraestructure/controllers/controllerGetRestaurantUser.ts
@@ -18,7 +18,7 @@ export class RestaurantControllerGetUser {
                 throw new Error('User ID (uuid) is missing in the request parameters.');
             }
 
-            const url = `http://serviceway-env.eba-2b4adbwi.us-east-1.elasticbeanstalk.com/api/v1/User//${uuid}`;
+            const url = `https://users.userconnectors.com/api/v1/User//${uuid}`;
             const response = await axios.get(url);
 
             console.log("Request successful. Data:", response.data);
